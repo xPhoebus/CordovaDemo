290bb712aec6f03ed749c4fa30249ac86c258b60
liuyq <liuyq@jingoal.com>
2018-06-25 14:44:15 +0800

    Add HybirdActivity

--- Andorid/app/src/main/java/com/pangustars/cordovademo/MainActivity.kt
+++ Andorid/app/src/main/java/com/pangustars/cordovademo/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.pangustars.cordovademo
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.pangustars.panguhybird.HybirdActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,8 +18,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            startActivity(Intent(this, HybirdActivity::class.java))
         }
     }
 
---
+++ Andorid/panguhybird/src/main/java/com/pangustars/panguhybird/HybirdActivity.kt
@@ -0,0 +1,11 @@
+package com.pangustars.panguhybird
+
+import android.app.Activity
+import android.os.Bundle
+
+class HybirdActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
