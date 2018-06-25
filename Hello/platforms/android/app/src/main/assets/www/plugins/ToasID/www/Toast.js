eafb59ed60511429f9765616964b92dcb7554831
liuyq <liuyq@jingoal.com>
2018-06-25 16:03:10 +0800

    Hello call Toast Plugin succeeded

--- Hello/platforms/android/app/src/main/assets/www/plugins/ToasID/www/Toast.js
+++ Hello/platforms/android/app/src/main/assets/www/plugins/ToasID/www/Toast.js
@@ -2,7 +2,7 @@ cordova.define("ToasID.Toast", function(require, exports, module) {
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Toast', 'coolMethod', [arg0]);
+    exec(success, error, 'Toast', 'toast', [arg0]);
 };
 
 });
