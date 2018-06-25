e679357ef74d01e44d63e5bed19e26eb2b30a32b
liuyq <liuyq@jingoal.com>
2018-06-25 14:45:37 +0800

    Modify HybirdActivity base clas to CordovaActivity

--- Andorid/panguhybird/src/main/java/com/pangustars/panguhybird/HybirdActivity.kt
+++ Andorid/panguhybird/src/main/java/com/pangustars/panguhybird/HybirdActivity.kt
@@ -2,8 +2,9 @@ package com.pangustars.panguhybird
 
 import android.app.Activity
 import android.os.Bundle
+import org.apache.cordova.CordovaActivity
 
-class HybirdActivity : Activity() {
+class HybirdActivity : CordovaActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
