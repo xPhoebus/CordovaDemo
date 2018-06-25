86e6d052adf85f476c60af060533feca9fb67072
liuyq <liuyq@jingoal.com>
2018-06-25 15:01:14 +0800

    import toast plugin to hello

--- Hello/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ Hello/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,9 +1,19 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+  {
+    "id": "ToasID.Toast",
+    "file": "plugins/ToasID/www/Toast.js",
+    "pluginId": "ToasID",
+    "clobbers": [
+      "cordova.plugins.Toast"
+    ]
+  }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3"
+  "cordova-plugin-whitelist": "1.3.3",
+  "ToasID": "1.0.0"
 };
 // BOTTOM OF METADATA
 });
