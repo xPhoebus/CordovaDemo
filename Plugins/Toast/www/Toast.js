19cd8a6a46d9f30aa50f055f3f42313d1ccdf3f7
liuyq <liuyq@jingoal.com>
2018-06-25 16:04:03 +0800

    correct toast plugin method called

--- Plugins/Toast/www/Toast.js
+++ Plugins/Toast/www/Toast.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'Toast', 'coolMethod', [arg0]);
+    exec(success, error, 'Toast', 'toast', [arg0]);
 };
